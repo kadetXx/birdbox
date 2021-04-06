672eb96c317163c49cb9278a4b2f83dadc4f521a
kadet <techkadet@gmail.com>
2021-04-06 14:22:55 +0100

    edited gender select

--- web/src/components/SetupAccount.vue
+++ web/src/components/SetupAccount.vue
@@ -22,7 +22,6 @@
           <option value="male">Male</option>
           <option value="Female">Female</option>
           <option value="Non binary">Non binary</option>
-          <option value="Prefer not to say">Prefer not to say</option>
         </select>
 
         <div class="form__btn-wrap">
